8a6588812ceb800da1591f22283292ef675647d4
A77lejandro <115905605+LihuelA@users.noreply.github.com>
2023-01-27 19:13:11 -0600

    alerts and creating variables

---
+++ javascript.js
@@ -0,0 +1,11 @@
+console.log("Hello, world!")
+let pirulin;
+pirulin = 'redonda';
+
+alert(pirulin)
+
+let lala = 'gaty'; //define the variable and assign a value at once
+alert(lala);
+
+let mau = 'miu', ru = 'mee', suaves = 'run run';
+alert(mau + ' ' + ru + ' ' + suaves);
