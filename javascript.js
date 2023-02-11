5a17a97d6afcbca5d2b46b3a359a2b550ec2e5cd
A77lejandro <115905605+LihuelA@users.noreply.github.com>
2023-02-11 12:21:14 -0600

    cambiados a párrafos en html los console logs

--- javascript.js
+++ javascript.js
@@ -47,10 +47,24 @@ admin = Name;
 alert(admin);
 
 const OUR_PLANET = 'Earth';
+function confirmAction() { //hay que sacar la función de adentro del for porque declarada adentro del for 
+  //pierde el scope el break
 for (let i = 0; i < 3; i++) {
-username = prompt("Please enter your username"); //acá voy a crear un loop para confirmar el nombre para practicar
-alert("Your name is " + username+ ' ¿es correcto?'); //voy a cambiar los console.log por alerts
+let username = prompt("Please enter your username"); //acá voy a crear un loop para confirmar el nombre para practicar
+let confirmAction = confirm("Your name is " + username + " ¿shall we continue?");//voy a cambiar los console.log por
+//alerts y acá el alert por un confirm button
+if (confirmAction == true) {
+  alert("Welcome "+ username);
+  break;
+} else {
+  alert("Ok, please enter your name again");
 }
+}
+}
+confirmAction(); //esto es muy importante para llamar y activar a la función, si no es ignorada. Y hacerlo afuera del loop
+//for para que todo se active
+
+
 /* Prompt the user to enter their birth day in the format "MM/DD/YYYY"
 let birthday = prompt("Please enter your birth day (MM/DD/YYYY)");
  Convert the user's birth day string into a date object
@@ -60,13 +74,16 @@ if (isNaN(birthdate.getTime())) {
 } isNan is not a number es lo que arrojaría como true si no es una fecha válida
 ESTE COMENTARIO ES CÓDIGO VIEJO DE PRÁCTICA PERO SIN EL LOOP NO ES ÚTIL EL CARTEL DE ERROR CON LA FECHA*/
 
+let birthday, birthdate; //super importante declarar las variables afuera y antes del loop for porque 
+//sino quedan adentro de su scope e inaccesibles fuera de ahí
 // Use a for loop to continuously prompt the user for their birthdate
 for (let i = 0; i < 3; i++) { //el let de acá sirve para crear dos variables, la del prompt que puede ser cualquier cosa
   //inclusive palabras y a partir del prompt una variable que debe ser una fecha o si no da isNaN not a number, o error
     birthday = prompt("Please enter your birth day (MM/DD/YYYY)"); //crea el prompt junto a un campo para ingresar datos de la var
     birthdate = new Date(birthday); //new Date(birthday) crea un objeto de fecha con sus horas minutos y segundos a partir
     //de lo que pongo entre paréntesis
-  
+    // OJOOO AQUÍ ES IMPORTANTE NO PONER LET AUNQUE ME LO PIDA VISUAL CODE PORQUE SI NO SE VUELVEN A DECLARAR LAS VARIABLES ADENTRO
+    //DEL LOOP Y AHÍ SE QUEDAN. lo que hay que hacer es quitar el let para que sólo se actualicen las variables ya declaradas
     // Check if the birthdate is a valid date
     if (!isNaN(birthdate.getTime())) { //si !NO es un isNaN o sea error entonces break, rompo el loop
       // If the birthdate is a valid date, break out of the loop
@@ -76,33 +93,55 @@ for (let i = 0; i < 3; i++) { //el let de acá sirve para crear dos variables, l
       alert("Invalid birth date. Please enter a date in the format MM/DD/YYYY.");
     }
   }
-  
+  alert(birthdate);
   // If a valid birthdate was not entered after three attempts, show an error message
   if (isNaN(birthdate.getTime())) {
-    console.error("Too many attempts. Please try again later.");
+    alert("Too many attempts. Please try again later.");
   } else {
-    console.log("Your birthdate is " + birthdate);
+    alert("Your birthdate is " + birthdate);
   }
-console.log("your birthday is "+ birthday);
+  document.addEventListener("DOMContentLoaded", function() {
+  let p1 = document.createElement("p");
+p1.innerHTML = "your birthday is "+ birthday;
+document.body.appendChild(p1);
+  });
+
+//document.write("your birthday is "+ birthday); aquí cambié al uso document.write para que quede todo en el html
+//ohh pero todo quedaba en la misma línea y no se entendía nada así que cambié a crear páragrafos en el body vacío del html
+//ohh pero me daba el error "Uncaught TypeError: Cannot read properties of null (reading 'appendChild')"
+//por eso tuve que agregar el código recomendado por chatgpt para que espere a que cargue la página antes de correr el script
+//esto se debía a que no había un body para crear el párrafo ya que el script se ejecuta antes
+
 // Get the current date
 
 let today = new Date();
-console.log("today is "+ today);
-
-
-console.log("your birth date is "+ birthdate);
+document.addEventListener("DOMContentLoaded", function() {
+  let p1 = document.createElement("p");
+p1.innerHTML = "today is "+ today +
+              "<br><br> your birth date is "+ birthdate;
+document.body.appendChild(p1);
+  });
 
 // Calculate the user's age in years
 let age = today.getFullYear() - birthdate.getFullYear();
-console.log("the current year is "+ today.getFullYear());
-console.log("the year you were born was "+ birthdate.getFullYear());
-console.log("your age taking the years into account is "+ age);
+document.addEventListener("DOMContentLoaded", function() {
+  let p1 = document.createElement("p");
+p1.innerHTML = "the current year is "+ today.getFullYear() +
+              "<br><br> the year you were born was " + birthdate.getFullYear() + 
+              "<br><br> your age taking the years into account is " + age;
+document.body.appendChild(p1);
+  });
 
 // Adjust the age if the user's birthday has not yet occurred this year
 let m = today.getMonth() - birthdate.getMonth(); // Here, we're calculating the difference
 //between the current month and the birth month of the user. The result is stored in the variable m.
-console.log("estamos en el mes " + today.getMonth());
-console.log("naciste en el mes de " + birthdate.getMonth());
+document.addEventListener("DOMContentLoaded", function() {
+let p1 = document.createElement("p");
+p1.innerHTML = "estamos en el mes " + today.getMonth() +
+              "<br><br> naciste en el mes de " + birthdate.getMonth() + 
+              "<br><br> your age taking the months into account is " + age;
+document.body.appendChild(p1);
+  });
 //ojoooo esto da menos uno al mes porque está basado en que enero es el mes cero wooow
 /* según chat gpt: The getMonth() method in JavaScript returns the month as a zero-based value, meaning
 that January is represented as 0, February is represented as 1, and so on. So, if the
@@ -124,18 +163,29 @@ aquí pondré el array*/
 const monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
 "Septiembre", "Octubre", "Noviembre", "Diciembre"
 ];
-console.log("El mes actual es: " + monthNames[today.getMonth()]);
-console.log("Naciste en el mes de: " + monthNames[birthdate.getMonth()]);
-console.log("la diferencia o resta entre el mes actual y el de tu nacimiento da: "+ m);
-console.log("El día de hoy es: " + today.getDate());
-console.log("el día de tu nacimiento fue: "+ birthdate.getDate());
+document.addEventListener("DOMContentLoaded", function() {
+  let p1 = document.createElement("p");
+  p1.innerHTML = "El mes actual es: " + monthNames[today.getMonth()] +
+                "<br><br> Naciste en el mes de: " + monthNames[birthdate.getMonth()] + 
+                "<br><br> la diferencia o resta entre el mes actual y el de tu nacimiento da: "+ m +
+                "<br><br> El día de hoy es: " + today.getDate() +
+                "<br><br> el día de tu nacimiento fue: "+ birthdate.getDate();
+  document.body.appendChild(p1);
+    });
 if (today.getDate() < birthdate.getDate()) {
-    console.log(today.getDate()+"<"+birthdate.getDate())
+  document.addEventListener("DOMContentLoaded", function() {
+    let p1 = document.createElement("p");
+    p1.innerHTML = "la fecha de hoy: " + today.getDate() +" es menor que "+ "tu fecha de nacimiento: " + birthdate.getDate();
+    document.body.appendChild(p1);
+      });
 } else {
-    console.log(today.getDate()+">"+birthdate.getDate())
+  document.addEventListener("DOMContentLoaded", function() {
+    let p1 = document.createElement("p");
+    p1.innerHTML = "la fecha de hoy: " + today.getDate() +" es mayor que "+ "tu fecha de nacimiento: " + birthdate.getDate();
+    document.body.appendChild(p1);
+      });
 }
 
-console.log(today.getDate()+">"+birthdate.getDate())
 if (m < 0 || (m === 0 && today.getDate() < birthdate.getDate())) { 
     age--;
 }
